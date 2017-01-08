74d192cc11a76b79d66f07dea91d66359c0f7245
xuehua <xuehua@gmail.com>
2017-01-08 12:07:12 -0800

    Added some codes related to notification.

--- ParseStarter/AppDelegate.swift
+++ ParseStarter/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Parse
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,12 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let configuration = ParseClientConfiguration {
             // Add your Parse applicationId:
-            $0.applicationId = "your_application_id"
+            $0.applicationId = "appid"
             // Uncomment and add your clientKey (it's not required if you are using Parse Server):
-            $0.clientKey = "your_client_key"
+            $0.clientKey = "Masterkey"
             
             // Uncomment the following line and change to your Parse Server address;
-            $0.server = "https://YOUR_PARSE_SERVER/parse"
+            $0.server = "http://myserver/parse"
             
             // Enable storing and querying data from Local Datastore.
             // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
@@ -37,9 +38,94 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         Parse.initialize(with: configuration)
         
+        PFUser.enableAutomaticUser()
+        
+        let defaultACL = PFACL()
+        
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.getPublicReadAccess = true
+        
+        PFACL.setDefault(defaultACL, withAccessForCurrentUser: true)
+        
+        if application.applicationState != UIApplicationState.background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+            
+            let oldPushHandlerOnly = !responds(to: #selector(UIApplicationDelegate.application(_:didReceiveRemoteNotification:fetchCompletionHandler:)))
+            var noPushPayload = false
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsKey.remoteNotification] == nil
+            }
+            if oldPushHandlerOnly || noPushPayload {
+                PFAnalytics.trackAppOpened(launchOptions: launchOptions)
+            }
+        }
+        
+        //------------------------------------------------------------------//
+        // Uncomment the below to enale push notifications, need to be an   //
+        // apple developer to test notification feature                     //
+        //------------------------------------------------------------------//
+        //  let types: UIUserNotificationType = [.alert, .badge, .sound]
+        //  let settings = UIUserNotificationSettings(types: types, categories: nil)
+        //  application.registerUserNotificationSettings(settings)
+        //  application.registerForRemoteNotifications()
+        
         return true
     }
+    
+    //--------------------------------------
+    // MARK: Push Notifications
+    //--------------------------------------
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let installation = PFInstallation.current()
+        installation?.setDeviceTokenFrom(deviceToken)
+        installation?.saveInBackground()
+        print("register")
+        PFPush.subscribeToChannel(inBackground: "") { (succeeded, error) in
+            if succeeded {
+                print("ParseStarterProject successfully subscribed to push notifications on the broadcast channel.\n")
+            } else {
+                print("ParseStarterProject failed to subscribe to push notifications on the broadcast channel with error = %@.\n", error!)
+            }
+        }
+    }
+    
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+        print("application:didFailToRegisterForRemoteNotificationsWithError: %@\n", error.localizedDescription)
+    }
+    
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        PFPush.handle(userInfo)
+        if application.applicationState == UIApplicationState.inactive {
+            PFAnalytics.trackAppOpened(withRemoteNotificationPayload: userInfo)
+        }
+    }
 
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you want to use Push Notifications with Background App Refresh
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+    //     if application.applicationState == UIApplicationState.Inactive {
+    //         PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+    //     }
+    // }
+    
+    //--------------------------------------
+    // MARK: Facebook SDK Integration
+    //--------------------------------------
+    
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you are using Facebook
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+    //     return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, session:PFFacebookUtils.session())
+    // }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
